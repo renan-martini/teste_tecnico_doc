75d05e53b6613069ed98d219fc69039b6da650d7
renan-martini <renan.martiniduarte@gmail.com>
2022-11-19 13:57:08 -0300

    fix: path to json file fixed

--- bundle.js
+++ bundle.js
@@ -12306,7 +12306,9 @@ var app = (function () {
   }
   return (async function () {
     const e = document.getElementById("app"),
-      t = (e.getAttribute("data-root") || "") + "/insomnia.json";
+      t =
+        (e.getAttribute("data-root") || "") +
+        "/teste_tecnico_doc/insomnia.json";
     window.INSOMNIA_URL = t;
     try {
       const n = await fetch(t, {
